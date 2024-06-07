54d2cb17a1eed0996130fead057effa000c742f3
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 17:44:23 +0300

    lab-ex4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void cube(int num) {
+    int result = num * num * num;
+    cout << result << endl;
+}
+
+int main() 
 {
+    int num;
+    cout << "enter number ";
+    cin >> num;
+
+    cube(num);
 
     system("pause");
     return 0;
