718956ef0d951d1ddbd18fb48d42a1a4e264144d
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 19:28:44 +0300

    12

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void cube(int num) {
-    int result = num * num * num;
-    cout << result << endl;
-}
+
+
 
 int main() 
 {
-    int num;
-    cout << "enter number ";
-    cin >> num;
-
-    cube(num);
-
+    
     system("pause");
     return 0;
 }
