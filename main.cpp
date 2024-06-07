a0d59b918bc225d13e1c6594cbaafa236b8e2dde
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 17:25:42 +0300

    lab-ex2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+void factorial(int num) {
+    int result = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+    }
+    std::cout << result << std::endl;
+}
+
+int main() {
+    int num;
+    cout << "enter a number  ";
+    cin >> num;
+
+    factorial(num);
 
     system("pause");
     return 0;
