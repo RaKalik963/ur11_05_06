5ebbacdc4ff628fadb4d352d32f1dfbf922a723a
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 18:01:24 +0300

    lab-ex6

--- main.cpp
+++ main.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 void num_checking(int num)
 {
-	if (num < 0)
+	if (num > 0)
 	{
+		cout << "your num is positive" << endl;
 
 	}
-
+	else if (num == 0)
+	{
+		cout << "your num is not positive and negative" << endl;
+	}
+	else
+	{
+		cout << "your num is negative" << endl;
+	}
 }
 int main()
 {
+	int num;
+	cout << "enter number ";
+	cin >> num;
 
+	num_checking(num);
     system("pause");
     return 0;
 }
