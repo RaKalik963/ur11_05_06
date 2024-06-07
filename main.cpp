6928407de0b89f8bf2260b331cc94a227f095932
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 19:28:20 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+void power(int num1, int num2) {
+    int result = 1;
+    for (int i = 0; i < num2; ++i) {
+        result *= num1;
+    }
+    cout << result << endl;
+}
 
+int main() {
+    int num1, num2;
+    cout << "enter the base of the degree ";
+    cin >> num1;
+    cout << "enter degree ";
+    cin >> num2;
+
+    power(num1, num2);
     system("pause");
     return 0;
 }
