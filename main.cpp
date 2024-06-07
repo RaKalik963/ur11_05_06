fce8f189870702df041c6b6224d00acf16197c11
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 19:55:12 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+void check_number(int num) {
+    bool is_prime = true;
 
+    if (num <= 1) {
+        is_prime = false;
+    }
+    else {
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+    }
+
+    if (is_prime) {
+        cout << number << " is prime numbeê" << endl;
+    }
+    else {
+        cout << number << " is not prime number" << endl;
+    }
+}
+
+int main() {
+    int num;
+    cout << "enter num ";
+    cin >> num;
+
+    check_number(num);
     system("pause");
     return 0;
 }
