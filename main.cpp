5a34225af98d7bb2e39a5f2a9216ec851950dcaa
RaKalik963 <andrejmahot646@gmail.com>
2024-06-07 16:58:49 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void rectangle(int N, int K)
 {
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < K; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
 
+int main() {
+    int N, K;
+    cout << "Enter height: ";
+    cin >> N;
+    cout << "Enter width: ";
+    cin >> K;
+
+    rectangle(N, K);
     system("pause");
     return 0;
 }
