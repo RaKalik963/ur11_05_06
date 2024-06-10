adb6831a80511ec9a419aeb5105db3f296f2da80
RaKalik963 <andrejmahot646@gmail.com>
2024-06-10 13:22:10 +0300

    dz-ex3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+bool is_perfect(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == num;
+}
 
-    system("pause");
+void find_perfect_numbers(int start, int end) {
+    bool found = false;
+    for (int num = start; num <= end; ++num) {
+        if (is_perfect(num)) {
+            if (!found) {
+                std::cout << "perfect numbers in the entered interval: ";
+                found = true;
+            }
+            cout << num << " ";
+        }
+    }
+    if (!found) {
+        cout << "there are no perfect numbers in the entered diapasone";
+    }
+    cout << endl;
+}
+
+int main() {
+    int start, end;
+    cout << "enter start of diapasone ";
+    cin >> start;
+    cout << "enter end of diapasone ";
+    cin >> end;
+
+    find_perfect_numbers(start, end);
+
     return 0;
 }
