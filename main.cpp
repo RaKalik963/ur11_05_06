7dc0397cfc5204c380580430b75fb3e442f7127d
RaKalik963 <andrejmahot646@gmail.com>
2024-06-10 12:47:32 +0300

    dz-ex4

--- main.cpp
+++ main.cpp
@@ -1,29 +1,126 @@
 #include <iostream>
 using namespace std;
-
-
-
-
-void displayCard(int rank, int suit) {
-    const char* ranks[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-    const char* suits[] = { "Hearts", "Diamonds", "Clubs", "Spades" };
-
-    if (rank < 0 || rank > 12 || suit < 0 || suit > 3) {
-        std::cout << "Invalid card input." << std::endl;
+// (char)xxx
+//spades 
+// - clubs
+// - hearts
+// - diamonds
+void play_card(int card, int suit)
+{
+    if (card == 2 || card == 3 || card == 4 || card == 4 || card == 5 || card == 6 || card == 7 || card == 8 || card == 9 || card == 10)
+    {
+        cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+        cout << (char)186 << card << "                " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "        " << (char)suit << "        " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                 " << (char)186 << "\n";
+        cout << (char)186 <<  "                " << card << (char)186 << "\n";
+        cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << "\n";
     }
-    else {
-        std::cout << ranks[rank] << " of " << suits[suit] << std::endl;
+    else if (card == 11)
+    {
+        cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+        cout << (char)186 << "J" << "                " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "        " << (char)suit << "        " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                " << "J" << (char)186 << "\n";
+        cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << "\n";
+    }
+    else if (card == 12)
+    {
+        cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+        cout << (char)186 << "Q" << "                " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "        " << (char)suit << "        " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                " << "Q" << (char)186 << "\n";
+        cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << "\n";
+    }
+    else if (card == 13)
+    {
+        cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+        cout << (char)186 << "K" << "                " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "        " << (char)suit << "        " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                " << "K" << (char)186 << "\n";
+        cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << "\n";
+    }
+    else if (card == 14)
+    {
+        cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << "\n";
+        cout << (char)186 << "A" << "                " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "        " << (char)suit << "        " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                 " << (char)186 << "\n";
+        cout << (char)186 << "                " << "A" << (char)186 << "\n";
+        cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << "\n";
     }
 }
 
-int main() {
-    int rank, suit;
 
-    std::cout << "Enter the rank (0-12): ";
-    std::cin >> rank;
-    std::cout << "Enter the suit (0-3): ";
-    std::cin >> suit;
+int main() 
+{
+    int card, suit;
 
-    displayCard(rank, suit);
+    cout << "choise suit of card" << endl; 
+    cout << (char)3 << "hearts(3)" << endl;
+    cout << (char)4 << "diamonds(4)" << endl;
+    cout << (char)5 << "clubs(5)" << endl;
+    cout << (char)6 << "spades(6)" << endl;
+
+    do {
+        cout << "Enter your choice: ";
+        cin >> suit;
+    } while (suit < 3 || suit > 6);
+
+    do {
+        cout << "Enter your card number (2-10, J-11, Q-12, K-13, A-14): ";
+        cin >> card;
+    } while (card < 2 || card > 14);
+
+    play_card(card, suit);
+    
+
+
+
+
+
+   /* for (size_t i = 0; i < 255; i++)
+    {
+        cout << "\t\t" << i << " - " << char(i) << endl;
+    }*/
+
     return 0;
 }
